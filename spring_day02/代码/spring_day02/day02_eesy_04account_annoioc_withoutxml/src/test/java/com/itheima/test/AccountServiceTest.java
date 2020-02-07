8011cc694fe5322261cbfcfd8f012d6b8aa3dbf7
docin <437195595@qq.com>
2020-02-07 18:20:06 +0800

    Day02

---
+++ spring_day02/代码/spring_day02/day02_eesy_04account_annoioc_withoutxml/src/test/java/com/itheima/test/AccountServiceTest.java
@@ -0,0 +1,86 @@
+package com.itheima.test;
+
+import com.itheima.domain.Account;
+import com.itheima.service.IAccountService;
+import config.SpringConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * 使用Junit单元测试：测试我们的配置
+ * Spring整合junit的配置
+ *      1、导入spring整合junit的jar(坐标)
+ *      2、使用Junit提供的一个注解把原有的main方法替换了，替换成spring提供的
+ *             @Runwith
+ *      3、告知spring的运行器，spring和ioc创建是基于xml还是注解的，并且说明位置
+ *          @ContextConfiguration
+ *                  locations：指定xml文件的位置，加上classpath关键字，表示在类路径下
+ *                  classes：指定注解类所在地位置
+ *              可以删掉重复的部分，新建一个类对象，按类型自动注入即可
+ *   当我们使用spring 5.x版本的时候，要求junit的jar必须是4.12及以上
+ */
+
+//就像controller调用service
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
+//@ContextConfiguration(locations = "classpath:bean.xml")
+public class AccountServiceTest {
+
+    @Autowired
+    private IAccountService as = null;
+
+
+    @Test
+    public void testFindAll() {
+        //3.执行方法
+        /**
+         *  重复部分
+            //1.获取容易
+            ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+            //2.得到业务层对象
+            IAccountService as = ac.getBean("accountService",IAccountService.class);
+         */
+        List<Account> accounts = as.findAllAccount();
+        for(Account account : accounts){
+            System.out.println(account);
+        }
+    }
+
+    @Test
+    public void testFindOne() {
+        //3.执行方法
+        Account account = as.findAccountById(1);
+        System.out.println(account);
+    }
+
+    @Test
+    public void testSave() {
+        Account account = new Account();
+        account.setName("test anno");
+        account.setMoney(12345f);
+        //3.执行方法
+        as.saveAccount(account);
+
+    }
+
+    @Test
+    public void testUpdate() {
+        //3.执行方法
+        Account account = as.findAccountById(4);
+        account.setMoney(23456f);
+        as.updateAccount(account);
+    }
+
+    @Test
+    public void testDelete() {
+        //3.执行方法
+        as.deleteAccount(4);
+    }
+}
---
+++ spring_day02/笔记/readme.txt
@@ -0,0 +1,7 @@
+spring第二天：spring基于注解的IOC以及IoC的案例
+1、spring中ioc的常用注解
+2、案例使用xml方式和注解方式实现单表的CRUD操作
+	持久层技术选择：dbutils
+3、改造基于注解的ioc案例，使用纯注解的方式实现
+	spring的一些新注解使用
+4、spring和Junit整合
